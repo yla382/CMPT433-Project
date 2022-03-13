1d8dcc693ab2d15fd18241e88240b5c78f285eb9
yla382 <yoonhlee0831@gmail.com>
2022-03-13 13:04:54 -0700

    node-beagle-browser connection established

--- rover/rover.c
+++ rover/rover.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <sys/sysinfo.h>
 #include "network.h"
 
 #define THREAD_NUM 1
 
 static pthread_t tids[THREAD_NUM];
 static bool continueProram = true;
+
+/*
+Function to get string containing program info such as runtime
+*/
+static char *getProgramStatus() {
+	struct sysinfo info;
+	sysinfo(&info);
+	
+	int hour, minute, seconds;
+	hour = (info.uptime / 3600);
+	minute = (info.uptime - (3600 * hour)) / 60;
+	seconds = (info.uptime - (3600 * hour)- (minute * 60));
+	
+	static char status[40];
+	memset(status, 0, sizeof(char)*40);
+	snprintf(status, 40, "update,%d:%d:%d", hour, minute, seconds);
+	return status;
+}
 
 
 static void *udpCommunication() {
@@ -18,6 +37,9 @@ static void *udpCommunication() {
 		if(strcmp(request, "QUIT") == 0) {
 			continueProram = false;
 			break; //exit the while loop
+		} else if(strcmp(request, "UPDATE") == 0) {
+			char *status = getProgramStatus();
+			sendResponse(status);
 		} else {
 			continue;
 		}
