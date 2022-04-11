beb078a44a1b1b22b35fc313ae605a7e3b19d1ef
yla382 <yoonhlee0831@gmail.com>
2022-04-10 19:45:33 -0700

    removed folders

--- LightSensor/sleep.h
+++
@@ -1,6 +0,0 @@
-#ifndef SLEEP_H_
-#define SLEEP_H_
-
-int delay(int milisecs);
-
-#endif
--- RoverMotor/rover_motor.h
+++
@@ -1,6 +0,0 @@
-void initGpioMotor(void);
-
-void turnAllMotors(void);
-void turnOffMotors(void);
-
-void sleep_ms(unsigned int delayMs);
